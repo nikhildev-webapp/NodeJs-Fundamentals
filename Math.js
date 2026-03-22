84cbb2b28d8659caa5174454ae1c8c0da5ba1068
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-03-22 16:23:52 +0530

    multiply two number using multiply method from math.js

--- Math.js
+++ Math.js
@@ -1,5 +1,7 @@
 //Modules in Node.js
 //create a add,sub,multply,divide module and used it app.js
 
+export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
-
+export const multply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import { add, sub } from './Math.js';
+import { add, multply, sub } from './Math.js';
 console.log('Subtract Module From Math.js File')
 console.log('Two Number Subtraction:', sub(4000, 44))
 console.log('Add two number using the Add module from Math.js file');
 console.log(add(3, 4))
-console.log('sucsse`')
+console.log('Mulitply two number using multiply module');
+console.log(multply(3,3))
