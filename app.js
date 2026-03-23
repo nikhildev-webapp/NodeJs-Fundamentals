44e013f79e88dacb2e643ccfe59bbd374b6c4f11
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-03-23 16:36:35 +0530

    divide

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import { add, multply, sub } from './Math.js';
+import { add, divide, multply, sub } from './Math.js';
 console.log('Subtract Module From Math.js File')
 console.log('Two Number Subtraction:', sub(4000, 44))
 console.log('Add two number using the Add module from Math.js file');
 console.log(add(3, 4))
 console.log('Mulitply two number using multiply module');
 console.log(multply(3,3))
+console.log('Divide tow number using Divide module from math')
+console.log(divide(50,5))
